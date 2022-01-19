057fbdc8251d4ab6531654b380e67642577908ef
siwon <solidesite@gmail.com>
2022-01-19 20:31:44 +0900

    게시판 검색 Dao 기능메소드 추가

--- java/project_3rd_package/Dao/Dao.java
+++ java/project_3rd_package/Dao/Dao.java
@@ -9,14 +9,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import jspexp.a05_th_member.vo.Member;
 import project_3rd_package.vo.*;
 
 public class Dao {
 	private Connection con;
 	private Statement stmt;
 	private PreparedStatement pstmt;
-	private ResultSet rs; 
+	private ResultSet rs;
+
 	// 2. 공통 메서드 선언(setConn())
 	public void setConn() throws SQLException {
 		// 1) 드라이버 연결
@@ -24,67 +24,64 @@ public class Dao {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
-			System.out.println("예외 처리:"+e.getMessage());
+			System.out.println("예외 처리:" + e.getMessage());
 		}
 		// 2) 특정 서버 연결
 		String info = "jdbc:oracle:thin:@localhost:1521:xe";
-		con = DriverManager.getConnection(info,"scott","tiger");
+		con = DriverManager.getConnection(info, "scott", "tiger");
 		System.out.println("접속 성공");
 	}
+
 	void closeRsc() {
-		if(rs!=null) { 
+		if (rs != null) {
 			try {
 				rs.close();
 			} catch (SQLException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-			} 
+			}
 		}
-		if(pstmt!=null) { 
+		if (pstmt != null) {
 			try {
 				pstmt.close();
 			} catch (SQLException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-			} 
-		}		
-		if(stmt!=null) { 
+			}
+		}
+		if (stmt != null) {
 			try {
 				stmt.close();
 			} catch (SQLException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-			} 
+			}
 		}
-		if(con!=null) { 
+		if (con != null) {
 			try {
 				con.close();
 			} catch (SQLException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-			} 
-		}		
+			}
+		}
 	}
-	
+
 	public Member login(String id, String pass) {
 		// 리턴할 객체 선언.
-		
+
 		Member m = null;
 		// 1. 연결공통메서드 호출
-		String sql = "select * \r\n"
-				+ "from member \r\n"
-				+ "where id=? and pass = ? ";
+		String sql = "select * \r\n" + "from member \r\n" + "where id=? and pass = ? ";
 		try {
 			setConn();
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, id);
 			pstmt.setString(2, pass);
 			rs = pstmt.executeQuery();
-			if( rs.next() ) {
-				m = new Member(rs.getString("id"), rs.getString("pass"), 
-						rs.getString("name"), rs.getString("nickname"),
-						rs.getString("grade"),rs.getInt("point"),
-						rs.getString("intro"));
+			if (rs.next()) {
+				m = new Member(rs.getString("id"), rs.getString("pass"), rs.getString("name"), rs.getString("nickname"),
+						rs.getString("grade"), rs.getInt("point"), rs.getString("intro"));
 			}
 			rs.close();
 			pstmt.close();
@@ -96,8 +93,8 @@ public class Dao {
 			closeRsc();
 		}
 		return m;
-	}	
-	
+	}
+
 	public void insertMember(Member ins) {
 		String sql = "INSERT INTO MEMBER values(?,?,?,?,?,?,?)";
 		try {
@@ -126,28 +123,112 @@ public class Dao {
 			// 6. 예외 처리..
 			System.out.println(e.getMessage());
 			closeRsc();
-			
-			
-		}finally {
+
+		} finally {
 			System.out.println("등록 처리 완료!!");
 		}
 
-	}	
-	
-/* 
-   public ArrayList<CriticBoard> boardList() {
-      ArrayList<CriticBoard> boardList = new ArrayList<CriticBoard>();
-      try {
-         setConn();
-         stmt = con.createStatement();
-         String sql = "SELECT * FROM criticBoard";
+	}
 
-         // 결과 객체
-         rs = stmt.executeQuery(sql);
+	// 게시판 목록 가져오기
+	public ArrayList<CriticBoard> boardList() {
+		ArrayList<CriticBoard> boardList = new ArrayList<CriticBoard>();
+		try {
+			setConn();
+			stmt = con.createStatement();
+			String sql = "SELECT * FROM criticBoard";
 
-         // while문
-         int num = 1;
-         while (rs.next()) {
+			// 결과 객체
+			rs = stmt.executeQuery(sql);
+
+			// while문
+			int num = 1;
+			while (rs.next()) {
+				CriticBoard b = new CriticBoard(rs.getInt("boardNum"), rs.getString("boardTitle"),
+						rs.getDate("boardRegiDate"), rs.getInt("boardHits"), rs.getInt("boardLike"),
+						rs.getString("boardContent"), rs.getString("boardUser"));
+				boardList.add(b);
+				System.out.println("제목 : " + rs.getString("boardTitle"));
+
+			}
+
+			// 자원 해제
+			rs.close();
+			stmt.close();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			if (rs != null) {
+				rs = null;
+			}
+			if (stmt != null) {
+				stmt = null;
+			}
+			if (con != null) {
+				con = null;
+			}
+		}
+		return boardList;
+	}
+
+	// 게시판 검색
+	public ArrayList<CriticBoard> boardSearch(String str) {
+		ArrayList<CriticBoard> boardList = new ArrayList<CriticBoard>();
+		try {
+			setConn();
+			stmt = con.createStatement();
+			String sql = "SELECT * FROM CRITICBOARD\r\n" + "WHERE boardTitle LIKE '%" + str + "%'";
+
+			// 결과 객체
+			rs = stmt.executeQuery(sql);
+
+			// while문
+			int num = 1;
+			while (rs.next()) {
+				CriticBoard b = new CriticBoard(rs.getInt("boardNum"), rs.getString("boardTitle"),
+						rs.getDate("boardRegiDate"), rs.getInt("boardHits"), rs.getInt("boardLike"),
+						rs.getString("boardContent"), rs.getString("boardUser"));
+				boardList.add(b);
+				System.out.println("제목 : " + rs.getString("boardTitle"));
+
+			}
+
+			// 자원 해제
+			rs.close();
+			stmt.close();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			if (rs != null) {
+				rs = null;
+			}
+			if (stmt != null) {
+				stmt = null;
+			}
+			if (con != null) {
+				con = null;
+			}
+		}
+		return boardList;
+	}
+
+	public ArrayList<String> getTitle() {
+		ArrayList<String> boardList = new ArrayList<String>();
+		try {
+			setConn();
+			stmt = con.createStatement();
+			String sql = "SELECT title FROM board";
+
+			// 결과 객체
+			rs = stmt.executeQuery(sql);
+
+			// while문
+			int num = 1;
+			while (rs.next()) {
 //            private int boardNum;
 //            private String title;
 //            private String name;
@@ -155,86 +236,37 @@ public class Dao {
 //            private int hits;
 //            private int recommend;
 //            private String content;
-        	 //int boardNum, String boardTitle, Date boardRegiDate, int boardHits, int boardLike,String boardContent
- 			
-        	 
-        	CriticBoard b = new CriticBoard(rs.getInt("boardNum"), rs.getString("boardTitle"),  rs.getDate("boardRegiDate"), rs.getInt("boardHits"), rs.getInt("boardLike"), rs.getString("boardContent"),rs.getString("boardUser"));
-            boardList.add(b);
-            System.out.println("제목 : "+rs.getString("boardTitle"));
-         
-         }
+				boardList.add(rs.getString(1));
+			}
 
-         // 자원 해제
-         rs.close();
-         stmt.close();
-         con.close();
-      } catch (SQLException e) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-         System.out.println(e.getMessage());
-         if (rs != null) {
-            rs = null;
-         }
-         if (stmt != null) {
-            stmt = null;
-         }
-         if (con != null) {
-            con = null;
-         }
-      }
-      return boardList;
-   }
-   
-   public ArrayList<String> getTitle() {
-      ArrayList<String> boardList = new ArrayList<String>();
-      try {
-         setConn();
-         stmt = con.createStatement();
-         String sql = "SELECT title FROM board";
+			// 자원 해제
+			rs.close();
+			stmt.close();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			if (rs != null) {
+				rs = null;
+			}
+			if (stmt != null) {
+				stmt = null;
+			}
+			if (con != null) {
+				con = null;
+			}
+		}
+		return boardList;
+	}
 
-         // 결과 객체
-         rs = stmt.executeQuery(sql);
-
-         // while문
-         int num = 1;
-         while (rs.next()) {
-//            private int boardNum;
-//            private String title;
-//            private String name;
-//            private int dates;
-//            private int hits;
-//            private int recommend;
-//            private String content;
-            boardList.add(rs.getString(1));
-         }
-
-         // 자원 해제
-         rs.close();
-         stmt.close();
-         con.close();
-      } catch (SQLException e) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-         System.out.println(e.getMessage());
-         if (rs != null) {
-            rs = null;
-         }
-         if (stmt != null) {
-            stmt = null;
-         }
-         if (con != null) {
-            con = null;
-         }
-      }
-      return boardList;
-   }
-   
-   // 등록
-   public void insertboard(CriticBoard cb) {
-	   //INSERT INTO criticBoard values(1,'스파이더맨: 노 웨이 홈 (쿠키, 베놈, 삼스파)',sysdate,50,3,'리뷰','필름닷');
-		String sql = "INSERT INTO criticBoard VALUES ("+cb.getBoardNum()+"','"+cb.getBoardTitle()+"','"
-				+cb.getBoardRegiDate()+"','"+cb.getBoardHits()+"','"+cb.getBoardLike()+","
-		+cb.getBoardContent()+","+cb.getBoardUser()+")";
+	// 등록
+	public void insertboard(CriticBoard cb) {
+		// INSERT INTO criticBoard values(1,'스파이더맨: 노 웨이 홈 (쿠키, 베놈,
+		// 삼스파)',sysdate,50,3,'리뷰','필름닷');
+		String sql = "INSERT INTO criticBoard VALUES (" + cb.getBoardNum() + "','" + cb.getBoardTitle() + "','"
+				+ cb.getBoardRegiDate() + "','" + cb.getBoardHits() + "','" + cb.getBoardLike() + ","
+				+ cb.getBoardContent() + "," + cb.getBoardUser() + ")";
 		try {
 			setConn();
 			// 자동커밋 방지
@@ -243,7 +275,8 @@ public class Dao {
 			stmt.executeUpdate(sql);
 			con.commit();
 			// 자원해제
-			stmt.close(); con.close();
+			stmt.close();
+			con.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -254,27 +287,26 @@ public class Dao {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 				if (rs != null) {
-		            rs = null;
-		         }
-		         if (stmt != null) {
-		            stmt = null;
-		         }
-		         if (con != null) {
-		            con = null;
-		         }
+					rs = null;
+				}
+				if (stmt != null) {
+					stmt = null;
+				}
+				if (con != null) {
+					con = null;
+				}
 			}
-			
+
 		}
-		
-		
+
 	}
-*/
-   public static void main(String[] args) {
-      // TODO Auto-generated method stub
 
-      Dao d1 = new Dao();
-      d1.insertMember(new Member("higirl","6666","김민지","밍지","일반사용자",0,"ㅎㅇㅎㅇ"));
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-   }
+		Dao d1 = new Dao();
+		d1.insertMember(new Member("higirl", "6666", "김민지", "밍지", "일반 사용자", 0, "ㅎㅇㅎㅇ"));
+
+	}
 
 }
